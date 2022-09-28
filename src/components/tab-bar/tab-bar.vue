e17baa1af671d7114fb0b10434a42f65d7b2229e
aliyu <1324149139@qq.com>
2022-09-28 15:52:08 +0800

    使用vant搭建 tabBar

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,57 +1,27 @@
 <template>
-  <div class="tab-bar">
-    <template v-for="(item,index) in localData" :key="item.text">
-      <div class="tab-bar-item" @click="switchTab(index,item)" :class="{active: currentIndex === index}">
-        <img v-if="currentIndex !== index" :src="getImgUrl(item.image)" alt="">
-        <img v-else :src="getImgUrl(item.imageActive)" alt="">
+  <van-tabbar v-model="currentIndex">
+    <template v-for="(item,index) in localData" key="index">
+      <van-tabbar-item :to="item.path">
         <span>{{item.text}}</span>
-      </div>
+        <template #icon>
+          <img v-if="currentIndex!== index" :src="getImgUrl(item.image)" alt="">
+          <img v-else :src="getImgUrl(item.imageActive)" alt="">
+        </template>
+      </van-tabbar-item>
     </template>
-  </div>
+  </van-tabbar>
+
 </template>
 
 <script setup>
 import localData from "@/assets/localData/localData"
 import getImgUrl from "@/utils/load_asset"
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 const currentIndex = ref(0)
-const router = useRouter()
-function switchTab(index, item) {
-  currentIndex.value = index
-  router.push(item.path)
-}
 </script>
 
 <style lang="less" scoped>
-.tab-bar {
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 55px;
-  display: flex;
-  border-top: 1px solid #666;
-
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    &.active {
+  :deep(.van-tabbar-item--active){
       color: var(--primary-color);
-    }
-
-    img {
-      height:35px;
-    }
-
-    .text {
-      font-size: 12px;
-      margin-top: 2px;
-    }
   }
-}
 </style>
