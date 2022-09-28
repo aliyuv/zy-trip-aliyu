b198ff753d75e41aa6b3b35c501f0414f03464d9
aliyu <1324149139@qq.com>
2022-09-28 15:53:05 +0800

    搭建home页面 上半部分

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import tabBar from './components/tab-bar/tab-bar.vue';
+
 </script>
 
 <template>
-  
+
   <div class="app">
-    <router-view></router-view>
+     <router-view></router-view>
     <tabBar></tabBar>
   </div>
 </template>
   
-<style scoped></style>
+<style scoped>
+
+</style>
