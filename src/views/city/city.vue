2f30ec2e1fd74011bfb9e5fc95e89f327c51cb3f
aliyu <1324149139@qq.com>
2022-09-30 00:16:08 +0800

    实现搜索 和 区域切换tab

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,12 +1,34 @@
 <template>
- <div>
-   <template v-for="item in 1000">
-     <div>测试文本{{item}}</div>
-   </template>
- </div>
+  <div class="city">
+    <van-search
+        v-model="searchValue"
+        show-action
+        placeholder="城市/区域/位置"
+        shape="round"
+        background="#FFFFFF"
+        @cancel="cancelClick"
+    >
+    </van-search>
+    <van-tabs v-model:active="tabsActive">
+      <van-tab title="国内·港澳台">内容 1</van-tab>
+      <van-tab title="海外">内容 2</van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script setup>
+import {ref} from "vue";
+import {useRouter} from "vue-router";
+
+const searchValue = ref("")
+
+const router = useRouter()
+
+const cancelClick = () => {
+  router.back()
+}
+
+const tabsActive = ref()
 </script>
 
 <style scoped lang="less">
