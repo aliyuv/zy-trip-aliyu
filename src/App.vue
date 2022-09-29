f066b5cb6c3ab786e525044d790c51ac2115971b
aliyu <1324149139@qq.com>
2022-09-29 14:23:47 +0800

    完成city页面的路由切换

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { computed, ref } from 'vue';
 import tabBar from './components/tab-bar/tab-bar.vue';
-
+import {useRoute} from "vue-router";
+const route = useRoute()
 </script>
 
 <template>
-
   <div class="app">
      <router-view></router-view>
-    <tabBar></tabBar>
+     <tab-bar v-if="!route.meta.hiddenTab"></tab-bar>
   </div>
 </template>
   
