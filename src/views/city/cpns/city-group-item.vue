44d1569abbf3b1aefd372ebe9bb8d4cb5a9d71be
aliyu <1324149139@qq.com>
2022-10-02 13:17:33 +0800

    修复动态索引fix@ 完成热门城市展示

--- src/views/city/cpns/city-group-item.vue
+++ src/views/city/cpns/city-group-item.vue
@@ -1,26 +1,46 @@
 <template>
   <section class="city-item">
-    <template v-for="(group,index) in groupData.cities" :key="index">
-      <section class="content-group_item">
-        <van-index-bar :sticky="false">
+    <van-index-bar :sticky="false" :index-list="index_bar_list" highlight-color="red">
+      <section class="hot-city">
+        <van-index-anchor index="热门"></van-index-anchor>
+         <template v-for="(item,index) in groupData.hotCities" :key="index">
+           <van-tag round type="primary" color="#fff4ec" text-color="#000" size="large">{{ item.cityName }}</van-tag>
+         </template>
+      </section>
+
+      <template v-for="(group,index) in groupData.cities" :key="index">
+        <section class="content-group_item">
           <van-index-anchor :index="group.group"/>
           <template v-for="(city,index) in group.cities" :key="index">
             <van-cell :title="city.cityName"/>
           </template>
-        </van-index-bar>
-      </section>
-    </template>
+        </section>
+      </template>
+    </van-index-bar>
   </section>
 </template>
 
 <script setup>
+import {computed} from "vue";
+
 const props = defineProps({
   groupData: {
     type: Object,
     default: () => ({})
   }
 })
+
+//字母列表
+const index_bar_list = computed(() => {
+  const list = props.groupData.cities.map(item => item.group)
+  list.unshift("#")
+  return list
+})
 </script>
 
 <style scoped lang="less">
+:deep(.van-tag--large){
+  --van-tag-large-padding: 5px 20px;
+  margin: 6px 10px;
+}
 </style>
