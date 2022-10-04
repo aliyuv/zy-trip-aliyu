0d25487028fc1847675a391817e8f958317c1709
aliyu <1324149139@qq.com>
2022-10-05 00:55:08 +0800

    增加search 页面

---
+++ src/views/search/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="search-pag">
+    <h2>开始时间: {{ $route.query.today }}</h2>
+    <h2>结束时间: {{ $route.query.tomorrow }}</h2>
+    <h2>当前城市: {{ $route.query.activeCityName }}</h2>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="less">
+
+</style>
