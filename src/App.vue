3c69cf21ff9174f275a13909543bd2074b2c3d5f
aliyu <1324149139@qq.com>
2022-10-07 12:10:23 +0800

    完成 loading 组件页面封装

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { computed, ref } from 'vue';
 import tabBar from './components/tab-bar/tab-bar.vue';
 import {useRoute} from "vue-router";
+import loading from "@/components/loading/loading.vue"
 const route = useRoute()
 </script>
 
 <template>
   <div class="app">
-     <router-view></router-view>
-     <tab-bar v-if="!route.meta.hiddenTab"></tab-bar>
+    <router-view></router-view>
+    <tab-bar v-if="!route.meta.hiddenTab"></tab-bar>
+    <loading></loading>
   </div>
 </template>
-  
-<style scoped>
 
+<style scoped>
 </style>
---
+++ src/components/loading/loading.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="loading">
+    <div class="container">
+      <img src="@/assets/img/home/full-screen-loading.gif" alt="">
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="less">
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 10;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, .2);
+
+  .container {
+    width: 104px;
+    height: 104px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: url(@/assets/img/home/loading-bg.png) 0 0 / 100% 100%;
+
+    img {
+      width: 70px;
+      height: 70px;
+      margin-bottom: 8px;
+    }
+  }
+}
+</style>
