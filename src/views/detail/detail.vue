732eaa254ceed4a54baf0edebbc6ab1d35b670a4
aliyu <1324149139@qq.com>
2022-10-07 21:52:04 +0800

    添加detail 页面 完成路由跳转

---
+++ src/views/detail/detail.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="detail">
+  <h1>detail</h1>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/views/home/cpn/home-navbar-serach-box.vue
+++ src/views/home/cpn/home-navbar-serach-box.vue
@@ -60,8 +60,8 @@
       </div>
       <div class="list">
         <template v-for="(item,index) in houseListData">
-          <house-list-v9 v-if="item.discoveryContentType === 9" :houseListData="item"></house-list-v9>
-          <house-list-v3 v-else-if="item.discoveryContentType === 3" :houseListData="item"></house-list-v3>
+          <house-list-v9 v-if="item.discoveryContentType === 9" :houseListData="item" @click="toDetailClick"></house-list-v9>
+          <house-list-v3 v-else-if="item.discoveryContentType === 3" :houseListData="item" @click="toDetailClick"></house-list-v3>
         </template>
       </div>
     </section>
@@ -157,6 +157,10 @@ const startSearch = () => {
       activeCityName: activeCityName.value.name
     }
   })
+}
+
+const toDetailClick = () => {
+  router.push("/detail")
 }
 </script>
 
