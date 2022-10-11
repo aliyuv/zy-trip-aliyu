aeea2094ae3f318b31733b74175a66236eb5c53c
aliyu <1324149139@qq.com>
2022-10-12 00:42:28 +0800

    再次封装 useScroll

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,15 +1,22 @@
 import {onMounted, onUnmounted, ref} from "vue";
 import {throttle} from "underscore";
 
-export default function useScrollTop() {
+export default function useScrollTop(element) {
+  let el = window
   const scrollHeight = ref(0)
   const clientHeight = ref(0)
   const scrollTop = ref(0)
   const isScrollBottoming = ref(false)
   const scrollListenHandler = throttle(() => {
-    scrollHeight.value = document.documentElement.scrollHeight
-    clientHeight.value = document.documentElement.clientHeight
-    scrollTop.value = document.documentElement.scrollTop
+    if (el === window) {
+      scrollHeight.value = document.documentElement.scrollHeight
+      clientHeight.value = document.documentElement.clientHeight
+      scrollTop.value = document.documentElement.scrollTop
+    } else {
+      scrollHeight.value = el.scrollHeight
+      clientHeight.value = el.clientHeight
+      scrollTop.value = el.scrollTop
+    }
     if (clientHeight.value + scrollTop.value >= scrollHeight.value) {
       console.log("到底部了");
       isScrollBottoming.value = true
@@ -17,11 +24,12 @@ export default function useScrollTop() {
   }, 250)
 
   onMounted(() => {
-    window.addEventListener("scroll", scrollListenHandler)
+    if (element) el = element.value
+    el.addEventListener("scroll", scrollListenHandler)
   })
 
   onUnmounted(() => {
-    window.removeEventListener("scroll", scrollListenHandler)
+    el.removeEventListener("scroll", scrollListenHandler)
   })
 
   return {scrollHeight, clientHeight, scrollTop, isScrollBottoming}
